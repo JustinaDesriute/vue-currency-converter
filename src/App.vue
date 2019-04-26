fbb60bf039b5d4682e689ef074cd25c9e41c1c44
JustinaDesriute <justina.d.lt@gmail.com>
2019-04-26 23:03:16 +0200

    added axios to make external API calls, added tooltip and snackbar

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">currency converter</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-tooltip bottom>
+          <v-btn
+            flat
+            href="https://www.linkedin.com/in/justina-de%C5%A1ri%C5%ABt%C4%97-966153101/"
+            target="_blank"
+            slot="activator"
+          >
+            <span class="mr-2">built by: Justina Desriute</span>
+          </v-btn>
+        go to LinkedIn profile
+      </v-tooltip>
     </v-toolbar>
 
     <v-content>
@@ -20,7 +23,13 @@
       <AutocompleteInput/>
       <AutocompleteInput/>
       <InputValidation/>
+      <ErrorHandler/>
     </v-content>
+
+    <v-footer class="pa-3">
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }} contact me on LINKEDIN or check out my GitHub profile</div>
+    </v-footer>
   </v-app>
 </template>
 
@@ -28,18 +37,43 @@
 import DatePicker from './components/DatePicker'
 import AutocompleteInput from './components/AutocompleteInput'
 import InputValidation from './components/InputValidation'
+import ErrorHandler from './components/ErrorHandler'
+
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     AutocompleteInput,
     DatePicker,
+    ErrorHandler,
     InputValidation,
   },
   data () {
     return {
       //
     }
-  }
+  },
+  mounted() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        console.log('respone from api', response);
+      })
+      .catch(error => { // Executes if an error occurs if code is not >= 200 && < 300
+        this.showError = true;
+        this.error     = error;
+      }),
+
+      axios
+      .get('https://api.exchangeratesapi.io/2019-04-26')
+      .then(response => {
+        console.log('respone from ANOTHER api', response);
+      })
+      .catch(error => { // Executes if an error occurs if code is not >= 200 && < 300
+        this.showError = true;
+        this.error     = error;
+      })
+  },
 }
 </script>
---
+++ src/components/ErrorHandler.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-btn
+        block
+        color="primary"
+        dark
+        @click="snackbar = true"
+      >
+        CLick to show Snackbar
+      </v-btn>
+    </v-card-text>
+
+    <v-snackbar
+      v-model="snackbar"
+      :bottom="y === 'bottom'"
+      :left="x === 'left'"
+      :multi-line="mode === 'multi-line'"
+      :right="x === 'right'"
+      :timeout="timeout"
+      :top="y === 'top'"
+      :vertical="mode === 'vertical'"
+    >
+      {{ text }}
+      <v-btn
+        color="pink"
+        flat
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'ErrorHandler',
+    data () {
+      return {
+        snackbar: false,
+        y: 'top',
+        x: null,
+        mode: '',
+        timeout: 6000,
+        text: 'Sorry, data did not come through :('
+      }
+    }
+  }
+</script>
