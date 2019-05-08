5d3228de57ac8e5dc17b97fba40f06b7be664c6c
Justina Desriute <justina.desriute@netcentric.biz>
2019-05-08 20:55:28 +0200

    added date in the future prevention

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-date-picker no-title color="red" width="250" @input="datePicked" v-model="picker" :landscape="landscape"></v-date-picker>
+  <v-date-picker :max="date" :min="min" no-title color="red" width="250" @input="datePicked" v-model="picker" :landscape="landscape"></v-date-picker>
 </template>
 
 <style src="./date-picker.css"></style>
@@ -13,6 +13,8 @@
         return {
           picker: new Date().toISOString().substr(0, 10),
           landscape: false,
+          date: new Date().toISOString().substr(0,10),
+          min: new Date(Date.now() - 315569260000).toISOString().substr(0,10),
         }
       },
 
